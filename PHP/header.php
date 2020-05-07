006c139b6b4214bebd573389895e49ab1e26e3e6
Rutvik1999 <ruchitprajapati2405@gmail.com>
2020-05-07 12:29:56 +0530

    now you just have to style all things. now i m going work onlog in  page database

--- PHP/header.php
+++ PHP/header.php
@@ -1,75 +1,52 @@
-
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="./Index.html" class="active">Home</a>
-          <div class="dropdown">
-               <button class="dropbtn">Smartphone
-                    <i class="fa fa-caret-down"></i>
-               </button>
-               <div class="dropdown-content">
-                    <a href="#">Oppo</a>
-                    <a href="#">Vivo</a>
-                    <a href="#">Redmi</a>
-                    <a href="#">Apple</a>
-                    <a href="#">Oneplus</a>
-               </div>
-          </div>
-          <a href="#news">Compare</a>
-          <a href="#contact">Contact Us</a>
-          <a href="#about">About</a>
-          <div class="dropdown">
-               <button class="dropbtn">
-                    <i class="fa fa-user"></i>
-               </button>
-               <div class="dropdown-content">
-                    <a href="./PHP/login.php">Login</a>
-                    <a href="./PHP/Register.php">Sign up</a>
-               </div>
-               <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-          <div class="search">
-               <form action="">
-                    <input type="text" placeholder="Search" name="search" required>
-                    <button type="submit">
-                         Search
-                    </button>
-               </form>
-        <button class="navbar-toggler"
-            type="button"
-                data-toggle="collapse"
-                data-target = "#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="mr-auto"></div>
-            <div class="navbar-nav">
-                <a href="cart.php" class="nav-item nav-link active">
-                    <h5 class="px-5 cart">
-                        <i class="fas fa-shopping-cart"></i> Cart
-                        <?php
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Smart Phone
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Samsung</a>
+                        <a class="dropdown-item" href="#">Oppo</a>
+                        <a class="dropdown-item" href="#">Vivo</a>
+                        <a class="dropdown-item" href="#">Redme</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" tabindex="-1">Sign Up</a>
+                </li>
+                    <a class="nav-link" href="#" tabindex="-1">Log In</a>
+                <li>
 
-                        if (isset($_SESSION['cart'])){
-                            $count = count($_SESSION['cart']);
-                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
-                        }else{
-                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
-                        }
+                </li>
+            </ul>
 
-                        ?>
-                    </h5>
-                </a>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="mr-auto"></div>
+                <div class="navbar-nav">
+                    <a href="cart.php" class="nav-item nav-link active">
+                        <h5 class="px-5 cart">
+                            <i class="fas fa-shopping-cart"></i> Cart
+                            <?php
+
+                            if (isset($_SESSION['cart'])) {
+                                $count = count($_SESSION['cart']);
+                                echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                            } else {
+                                echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                            }
+
+                            ?>
+                        </h5>
+                    </a>
+                </div>
             </div>
-        </div>
 
     </nav>
 </header>
-
-
-
-
-
-
